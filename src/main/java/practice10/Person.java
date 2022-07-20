19d1a2e99e15ba35642de13092dcea117e6ede26
YunChen <1183728554@qq.com>
2022-07-20 22:03:01 +0800

    fest:Finish practice 10 and passed all the test examples.

--- src/main/java/practice10/Person.java
+++ src/main/java/practice10/Person.java
@@ -1,4 +1,47 @@
 package practice10;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Setter
+@Getter
 public class Person {
+    private int id;
+    private String name;
+    private int age;
+    /**
+     * Lombok needs a default constructor in the base class
+     */
+    public Person() { }
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public Person(int id,String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id &&
+                age == person.age &&
+                Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age);
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old.",this.name,this.age);
+    }
 }
