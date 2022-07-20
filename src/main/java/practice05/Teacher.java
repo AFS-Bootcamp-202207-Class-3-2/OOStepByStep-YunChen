a83067669d05e7921e7016f50cc5130047578e1b
YunChen <1183728554@qq.com>
2022-07-20 16:26:22 +0800

    feat:Finished practice5 and passed the test example.

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,28 @@
 package practice05;
 
-public class Teacher {
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper=false)
+public class Teacher extends Student {
+    public Teacher(String name, int age,int klass) {
+        super(name, age,klass);
+    }
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        return this.getKlass()!=0?
+                String.format("My name is %s. I am %d years old. I am a Teacher. I teach Class %d.",
+                        this.getName(),
+                        this.getAge(),
+                        this.getKlass()):
+                String.format("My name is %s. I am %d years old. I am a Teacher. I teach No Class.",
+                        this.getName(),
+                        this.getAge());
+    }
 }
